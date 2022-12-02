92bdaab22ea325dee7e227e27846c11b1311c1d8
Marina Panina <paninamar235@gmail.com>
2022-12-02 13:36:48 +1000

    "For first review"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,8 +4,6 @@ package jm.task.core.jdbc;
 public class Main {
 
     public static void main(String[] args) {
-        //Честно говоря я так и не поняла сокрального смысла такой архитектуры приложения
-        //и как Connection реализовать в try with resources
-        //ну и с автогенирацией id вопросики
+        //проблемы с генерацией id(?)
     }
 }
