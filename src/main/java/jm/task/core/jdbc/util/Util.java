b4c3204b8d9ee16ad4fe55df2072bba55e37334b
Marina Panina <paninamar235@gmail.com>
2022-12-06 01:19:05 +1000

    "Add files to gitignore"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -57,7 +57,7 @@ public class Util {
 
                 sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             } catch (Exception e) {
-                e.printStackTrace();
+                 e.printStackTrace();
             }
         }
         return sessionFactory;
